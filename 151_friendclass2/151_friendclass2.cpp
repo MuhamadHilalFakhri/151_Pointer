251db1f96508d9506ce5bbdf596f9c5d69b2f1cf
hilal555 <muhamadhilal04@gmail.com>
2023-05-08 15:52:33 +0700

    membuat class

--- 151_friendclass2/151_friendclass2.cpp
+++ 151_friendclass2/151_friendclass2.cpp
@@ -5,4 +5,18 @@ using namespace std;
 class mahasiswa {
 private:
 	string nama;
+public:
+	friend void setNama(mahasiswa& a, string);
 };
+
+void setNama(mahasiswa& a, string pNama) {
+	a.nama = pNama;
+	cout << "Nama: " << a.nama;
+}
+int main()
+{
+	mahasiswa mhs;
+	setNama(mhs, "Joko Kumat");
+	cout << endl;
+	return 0;
+}
